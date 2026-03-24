14b8e1a5e3d9cdb1a4e886fb144b4270f4c34eda
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-03-24 22:25:06 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -75,9 +75,9 @@ if [ ! -f sbx_update ]; then
 echo "执行必要的脚本依赖中，请稍后"
 if command -v apk >/dev/null 2>&1; then
 apk update >/dev/null 2>&1
-apk add gcompat libc6-compat >/dev/null 2>&1
+apk add busybox-static gcompat libc6-compat >/dev/null 2>&1
 elif command -v apt >/dev/null 2>&1; then
-apt update >/dev/null 2>&1 && apt install coreutils util-linux -y >/dev/null 2>&1
+apt update >/dev/null 2>&1 && apt install busybox-static coreutils util-linux -y >/dev/null 2>&1
 fi
 touch sbx_update
 fi
