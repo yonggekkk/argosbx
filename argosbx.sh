e608f1d0ea68161d21efea5bd0412dc68944fe4d
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-06-26 14:28:56 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -1510,10 +1510,10 @@ echo "💣【 Naiveproxy 】节点信息如下："
 port_nv=$(cat "$HOME/agsbx/port_nv")
 nv2_link="naive+https://$uuid:$uuid@$add:$port_nv?security=tls&sni=$sni&insecure=0&allowInsecure=0#${sxname}naive-h2-$hostname"
 nv3_link="naive+quic://$uuid:$uuid@$add:$port_nv?congestion_control=bbr&security=tls&sni=$sni&insecure=0&allowInsecure=0#${sxname}naive-h3-$hostname"
-echo "$nv_link2" >> "$HOME/agsbx/jhsub.txt"
-echo "$nv_link3" >> "$HOME/agsbx/jhsub.txt"
-echo "$nv_link2"
-echo "$nv_link3"
+echo "$nv2_link" >> "$HOME/agsbx/jhsub.txt"
+echo "$nv3_link" >> "$HOME/agsbx/jhsub.txt"
+echo "$nv2_link"
+echo "$nv3_link"
 echo
 sbnvpt(){
 cat <<EOF
